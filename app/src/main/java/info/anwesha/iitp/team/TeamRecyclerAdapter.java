ecdc9dfe88dd790b51f9b2a456f01b3c8da67848
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-01-07 03:02:19 +0530

    Optimize Imports

--- app/src/main/java/info/anwesha/iitp/team/TeamRecyclerAdapter.java
+++ app/src/main/java/info/anwesha/iitp/team/TeamRecyclerAdapter.java
@@ -1,8 +1,6 @@
 package info.anwesha.iitp.team;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
